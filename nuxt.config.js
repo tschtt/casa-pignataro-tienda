b906233d68351bcfc9b50d5a0fad9b2b947bf756
santiago <santiagotaschetta@gmail.com>
2021-11-25 11:23:08 -0300

    added description meta header

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Desde Casa Pignataro estamos trabajando para que tengas la mejor experiencia. Proximamente encontrarás nuestro catálogo virtual' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
