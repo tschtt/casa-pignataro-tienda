0d1b01260595ce1c1df42e450b9c8b38e6109c08
santiago <santiagotaschetta@gmail.com>
2021-11-24 22:16:17 -0300

    rebuild pagina sitio en construccion

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="main-container">
+  <div class="main-container background-image stack">
     <header>
       <h1>
-        <img class="logo" src="/logo-transparent.svg" alt="Casa Pignataro" width="350">
+        <img class="logo" src="/logo-transparent.svg" alt="Casa Pignataro" width="300">
       </h1>
     </header>
-    <main>
-      <h2>
+    <main class="stack">
+      <h2 class="border-left-blue">
         <b>Sitio</b> en <br/> construcción
       </h2>
       <p>
@@ -16,21 +16,23 @@
         virtual
       </p>
     </main>
-    <footer>
-      <h3>Contacto</h3>
-      <dl class="flex flex-column">
-        <div class="[ icon icon-place ][ flex flex-align-center ]">
+    <footer class="stack">
+      <h3 class="color-main">
+        Contacto
+      </h3>
+      <dl class="text-100">
+        <div class="[ icon icon-place ][ flex flex-center ]">
           <dt class="hidden">Ubicación</dt>
           <dd>Avenida Gral. San Martín 3704. Lomas del Mirador</dd>
         </div>
-        <div class="[ icon icon-schedule ][ flex flex-align-center ]">
+        <div class="[ icon icon-schedule ][ flex flex-center ]">
           <dt class="hidden">Horario</dt>
           <div>
-            <dd class="flex-grow-1">Lunes a Viernes de 08:30 a 13:33 - 15:00 a 18:00</dd>
-            <dd class="flex-grow-1">Sabados de 08:30 a 13:00</dd>
+            <dd>Lunes a Viernes de 08:30 a 13:33 - 15:00 a 18:00</dd>
+            <dd>Sabados de 08:30 a 13:00</dd>
           </div>
         </div>
-        <div class="[ icon icon-phone ][ flex flex-align-center ]">
+        <div class="[ icon icon-phone ][ flex flex-center ]">
           <dt class="hidden">Teléfono</dt>
           <dd>4454-5871</dd>
         </div>
@@ -39,136 +41,57 @@
   </div>
 </template>
 
-<script>
-export default {
-
-}
-</script>
-
 <style lang="scss">
 
 .main-container {
-  background-image: url('/construccion-fondo.jpg');
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: left;
-  background-color: black;
-  color: white;
-
   display: grid;
-  grid-template-rows: auto 1fr auto;
-  
-  height: 100%;
+  padding: 5vh var(--space-600);
+  min-height: 100%;
+  color: white;  
+  gap: var(--space-400);
 
-  > header {
-    margin-bottom: 1em;
-  }
-
-  > main {
-    font-size: 18px;
-    align-self: center;
-    padding: 1em 1.5em;
+  dl > * + * {
+    margin-top: var(--space-200);
   }
 
   > footer {
-    padding: 1em 1.5em;
+    align-self: center;
   }
 
-  .logo {
-    margin: 0 auto;
-  }
-
-  img {
-    // margin: auto;
-  }
-
-  h2 {
-    font-size: 45px;
+  .border-left-blue {
+    border-left: var(--space-300) solid var(--color-secondary);
+    padding-left: var(--space-300);
     margin-bottom: 1rem;
   }
 
-  h3 {
-    font-size: 30px;
-    margin-bottom: 1rem;
-  }
+}
 
-  b, h3 {
-    color: #f57733;
-  }
+.background-image {
+  background-image: url('/construccion-fondo.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: top;
 }
 
 @media (min-width: 600px) {
-  
+
   .main-container {
-    background-image: url('/construccion-fondo.jpg');
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: right;
-    
-    background-color: black;
-    color: white;
-    font-size: 20px;
-    
-    .logo {
-      margin: 0;
-      width: 450px;
-    }
-  
-    h1 {
-      margin-bottom: -1em;
-    }
-  
-    h2 {
-      border-left: 1rem solid #0546ec;
-      padding: 0 1rem;
-    }
-  
-    p {
-      margin-top: 1em;
-      font-size: 25px;
-      max-width: 40ch;
-    }
-  
-    b, h3 {
-      color: #f57733;
-    }
-  
-    b {
-      font-size: 75px;
-    }
+    gap: var(--space-400);
+  }
 
-    h2 {
-      font-size: 60px;
-    }
-
-    h3 {
-      font-size: 40px;
-    }
-  
-    > header {
-      margin-top: 1em;
-      margin-inline: 1em;
-      padding-inline: 1.5em;
-    }
-  
-    > main {
-      margin-inline: 2em;
-      padding-inline: 2em;
-    }
-  
-    > footer {
-      max-width: 80ch;
-      // border-top: 3px solid #0546ec;
-      border-top: 10px solid;
-      border-image-slice: 1;
-      border-width: 3px;;
-      border-image-source: linear-gradient(to right, hsla(223, 96%, 47%, 0.1), #0546ec 5%, hsla(223, 96%, 47%, 0.1) 95%);
-      margin-inline: 2em;
-      padding-block: 1em;
-    }
+  .main-container > footer {
+    align-self: initial;
+    
+    border-top: 10px solid;
+    border-image-slice: 1;
+    border-width: 3px;;
+    border-image-source: linear-gradient(to right, transparent, var(--color-secondary) 5%, transparent 80%);
+    padding-top: var(--space-400);
   }
   
+  .main-container .logo {
+    width: 350px;
+  }
 }
-
 
 </style>
