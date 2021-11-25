dc3cb88c9c990b42361fb57e0a4d2ddb289fe271
santiago <santiagotaschetta@gmail.com>
2021-11-24 22:31:03 -0300

    cambio en titulo de la página

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'casa-pignataro-tienda',
+    title: 'Casa Pignataro | Electrodomesticos',
     htmlAttrs: {
       lang: 'en'
     },
